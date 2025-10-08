4c75c02e4763a1c44c46c4a36c1cfeea64724c80
Thomas Viard <thomas.viard@epitech.eu>
2025-10-08 12:04:12 +0200

    Initialize and run HttpServer in Program.cs

--- Master/Program.cs
+++ Master/Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using Master.Services;
+
 Console.WriteLine("Hello, World!");
+
+try
+{
+    var server = new HttpServer("http://localhost:8080/");
+    await server.StartAsync();
+}
+catch (Exception ex)
+{
+    Console.WriteLine($"Failed to start server: {ex.Message}");
+}
