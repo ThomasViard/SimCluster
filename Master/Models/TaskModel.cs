0d16ed5bc137085a9c74d9387474f7474c0c5a5d
ThomasViard <91726845+ThomasViard@users.noreply.github.com>
2025-11-07 19:52:03 +0100

    refactor(master): migrate from HttpListener to ASP.NET Core
    Breaking changes:
    - Change SDK from Microsoft.NET.Sdk to Microsoft.NET.Sdk.Web
    - Remove custom HttpServer and RouteManager classes
    - Implement WebApplication with Controllers and DI
    - Add PrefixedConsoleWriter for [Master] logging
    - Add required modifier to TaskModel properties
    
    New architecture:
    - ASP.NET Core Minimal API with Controllers
    - Dependency Injection container
    - IHostedService for background tasks
    - Graceful shutdown with ApplicationStopping hook

--- Master/Models/TaskModel.cs
+++ Master/Models/TaskModel.cs
@@ -3,8 +3,8 @@ namespace Master.Models;
 public class TaskModel
 {
     public int Id { get; set; }
-    public string Title { get; set; }
-    public string Description { get; set; }
+    public required string Title { get; set; }
+    public required string Description { get; set; }
     public DateTime DueDate { get; set; }
     public bool IsCompleted { get; set; }
 }
--- Master/Program.cs
+++ Master/Program.cs
@@ -1,11 +1,41 @@
-﻿using Master.Services;
+﻿using Common;
+using Master.Services;
 
-try
+Console.SetOut(new PrefixedConsoleWriter(Console.Out, "[Master] "));
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Logging.ClearProviders();
+builder.Logging.SetMinimumLevel(LogLevel.None);
+
+builder.Services.AddControllers();
+
+// Services
+builder.Services.AddSingleton<WorkerRegistry>();
+builder.Services.AddScoped<IWorkerManagementService, WorkerManagementService>();
+builder.Services.AddSingleton<ShutdownNotificationService>();
+builder.Services.AddHttpClient();
+
+// Background Services
+builder.Services.AddHostedService<WorkerMonitoringService>();
+
+var app = builder.Build();
+
+app.MapControllers();
+
+var lifetime = app.Services.GetRequiredService<IHostApplicationLifetime>();
+lifetime.ApplicationStopping.Register(() =>
 {
-    var server = new HttpServer("http://localhost:8080/");
-    await server.StartAsync();
-}
-catch (Exception ex)
-{
-    Console.WriteLine($"Failed to start server: {ex.Message}");
-}
+    Console.WriteLine("Master shutdown initiated...");
+
+    // Notifier tous les Workers de l'arrêt
+    using var scope = app.Services.CreateScope();
+    var shutdownService = scope.ServiceProvider.GetRequiredService<ShutdownNotificationService>();
+    shutdownService.NotifyAllWorkersAsync().GetAwaiter().GetResult();
+
+    Console.WriteLine("Master stopped");
+});
+
+Console.WriteLine("Starting Master on http://localhost:8080");
+
+app.Run("http://localhost:8080");
